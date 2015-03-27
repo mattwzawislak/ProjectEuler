e7e436626855995f134ebbb373afd279c5231bf9
Obicere <mattwzawislak@gmail.com>
2015-03-27 14:05:46 -0600

    Fuck tabs

--- src/page1/Euler001.s
+++ src/page1/Euler001.s
@@ -1,46 +1,46 @@
 print:
-	.asciz	"Sum: %d\n"
+    .asciz  "Sum: %d\n"
 
-	.align 	4
+    .align  4
 
-	.global	main
+    .global main
 main:
-	save	%sp,	-92,	%sp
+    save    %sp,    -92,    %sp
 
-	clr		%l0
-	mov		3,		%l1
+    clr     %l0
+    mov     3,      %l1
 
 loop:
 
-	mov		%l1,	%o0
-	call	.rem
-	mov		3,		%o1
+    mov     %l1,    %o0
+    call    .rem
+    mov     3,      %o1
 
-	cmp		%o0,	0
-	be		yes
-	nop
+    cmp     %o0,    0
+    be      yes
+    nop
 
-	mov		%l1,	%o0
-	call	.rem
-	mov		5,		%o1
+    mov     %l1,    %o0
+    call    .rem
+    mov     5,      %o1
 
-	cmp		%o0,	0
-	be		yes
-	nop	
-	ba		post
-	nop
+    cmp     %o0,    0
+    be      yes
+    nop 
+    ba      post
+    nop
 
 yes:
-	add		%l0,	%l1,	%l0	
+    add     %l0,    %l1,    %l0	
 post:
 
-	cmp		%l1,	999
-	bl		loop
-	inc		%l1
+    cmp     %l1,    999
+    bl      loop
+    inc     %l1
 
-	set		print,	%o0
-	call	printf
-	mov		%l0,	%o1
+    set     print,  %o0
+    call    printf
+    mov     %l0,    %o1
 
-	mov		1,		%g1
-	ta		0
+    mov     1,      %g1
+    ta      0
